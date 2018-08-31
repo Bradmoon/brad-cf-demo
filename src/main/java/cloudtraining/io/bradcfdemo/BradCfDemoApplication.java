9e92afe763a2b5b50e169f554b7300d73c7db224
bradmoon <blv.moon@gmail.com>
2018-08-30 19:55:43 -0700

    initial upload

---
+++ src/main/java/cloudtraining/io/bradcfdemo/BradCfDemoApplication.java
@@ -0,0 +1,12 @@
+package cloudtraining.io.bradcfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradCfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradCfDemoApplication.class, args);
+	}
+}
